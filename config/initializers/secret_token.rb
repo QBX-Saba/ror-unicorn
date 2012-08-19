2665b9c56b4f1d20d0c6965fafc1ecbc4d36082d
Rustam Zagirov <rustam@zagirov.name>
2012-08-19 12:33:43 +0400

    start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Unicorn::Application.config.secret_token = '4672e200f21ae5b7bea4f1ffdf601d44db34c33b46bb8f7e1dd09f72e19d876ae0c39c0b608642e622c544cd0f217797e5ea4dab068f806d98a6b08bc45bc0a2'
